a5a143fe5899050353baf248a34c01164ccc4078
kotetuco <paprapapra@gmail.com>
2023-03-25 10:47:29 +0900

    Add Xcode project to Git repository.

---
+++ GMSSample/GMSSample/GMSSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GMSSampleApp.swift
+//  GMSSample
+//
+//  Created by kotetu on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GMSSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
